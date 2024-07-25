055aaabca30afbf12ce27101a7df22525703b02c
Joyrudra Biswas <joyrudra@itobuz.com>
2024-07-25 19:49:27 +0530

    feat. : tracking loading events for aframe

--- src/app/ui/3d/explore3d/explore.3d.tsx
+++ src/app/ui/3d/explore3d/explore.3d.tsx
@@ -1,29 +1,50 @@
-// @ts-nocheck
-import React from 'react';
+// Explore3D.tsx
+import React, { useEffect, useState } from 'react';
+import Loader from '../../global/loaders/moon.loader/circle.loader'; // Import the Loader component
 
 export default function Explore3D() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const handleModelLoad = () => {
+      setLoading(false); // Hide loader when model is loaded
+    };
+
+    document
+      .getElementById('moonModel')
+      ?.addEventListener('loaded', handleModelLoad);
+
+    () => {
+      document
+        .getElementById('moonModel')
+        ?.removeEventListener('loaded', handleModelLoad);
+    };
+  }, []);
+
   return (
     <div className="snap-section relative h-screen w-screen">
+      {loading && <Loader />} {/* Show loader if loading is true */}
       <a-scene
         embedded
         style={{ pointerEvents: 'none', userSelect: 'none' }}
         className="absolute inset-0"
-        vr-mode-ui="enabled: false" // Disable VR mode UI
-        keyboard-shortcuts="enabled: false" // Disable default keyboard shortcuts
+        vr-mode-ui="enabled: false"
+        keyboard-shortcuts="enabled: false"
       >
         <a-assets>
           <a-asset-item
-            id="model"
+            id="moonModel"
             src="./models/moon/scene.gltf"
+            // onLoad={handleModelLoad} // Event handler when model is loaded
           ></a-asset-item>
         </a-assets>
 
         <a-entity
-          gltf-model="#model"
-          position="0 1.5 -5"
+          gltf-model="#moonModel"
+          position="0 1 -5"
           scale="0.35 0.35 0.35"
           rotation="0 0 0"
-          animation="property: rotation; to: 0 360 0; loop: true; dur: 30000"
+          animation="property: rotation; to: 0 360 0; loop: true; dur: 100000"
         ></a-entity>
 
         <a-light
@@ -32,12 +53,6 @@ export default function Explore3D() {
           intensity="2"
           position="-30 15 5"
         ></a-light>
-
-        <a-camera
-          position="0 1.6 0"
-          look-controls="enabled: false"
-          wasd-controls="enabled: false"
-        ></a-camera>
       </a-scene>
     </div>
   );
---
+++ src/app/ui/global/loaders/moon.loader/circle.loader.tsx
@@ -0,0 +1,9 @@
+import './_circle.loader.scss';
+
+export default function MoonLoader() {
+  return (
+    <div className="loader">
+      <div className="spinner" />
+    </div>
+  );
+}
