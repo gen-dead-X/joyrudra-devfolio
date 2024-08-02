6ed6fee47ba42b46e1d5c0a4ff9c3e2dffcbca0d
Joyrudra Biswas <joyrudra@itobuz.com>
2024-08-02 17:57:10 +0530

    feat. : added profile

--- src/app/ui/dynamic.island/dynamic.island.navbar.tsx
+++ src/app/ui/dynamic.island/dynamic.island.navbar.tsx
@@ -74,19 +74,29 @@ export default function DynamicIslandNavbar() {
           </motion.div>
         </motion.div>
         <div className="flex gap-5 px-20 py-5">
-          <div className="h-[20rem] w-[15rem] overflow-hidden rounded-3xl object-cover">
+          <div
+            data-name="left"
+            className="h-[20rem] w-[15rem] overflow-hidden rounded-3xl object-cover"
+          >
             <img
               src="https://images.pexels.com/photos/3533228/pexels-photo-3533228.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
               alt="profile-image"
               className="h-full w-full object-cover"
             />
           </div>
-          <div>
-            <div>
-              <div className="relative z-0 h-3 w-3 animate-pulse rounded-full bg-green-500">
+          <div data-name="right" className="text-white dark:text-black">
+            <div className="flex items-center gap-2">
+              <div className="relative z-0 h-3 w-3 animate-pulse rounded-full bg-green-400">
                 <div className="absolute bottom-0 left-0 right-0 top-0 z-10 h-5 w-5 animate-ping" />
               </div>
-              <p>Available For Work</p>
+              <p className="bg-green-700/30 text-xs text-green-400">
+                Available For Work
+              </p>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <h4 className="text-3xl">Joyrudra Biswas</h4>
+              <p>Full Stack Developer</p>
             </div>
           </div>
         </div>
